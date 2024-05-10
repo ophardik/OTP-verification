3dab34ffcf64383532f6bbdffcab379079adbc94
ophardik <116378316+ophardik@users.noreply.github.com>
2024-05-10 09:57:25 +0530

    Update testing.js

--- test/testing.js
+++ test/testing.js
@@ -5,7 +5,7 @@ const server = require("../index.js");
 
 chai.should();
 chai.use(chaiHttp);
-//const number = process.env.TWILIO_MOBILE_NNUMBER; 
+
 describe("OTP coming", () => {
   it("Should send OTP to the given mobile number", async() => {
     
@@ -15,13 +15,12 @@ describe("OTP coming", () => {
       .post("/api/send-otp")
       .send({ phoneNumber: "+919828609896" });
 
-      //res.should.have.status(200, `Expected status code 200 but got ${res.status}. Response body: ${JSON.stringify(res.body)}`);
-     
+      
       res.should.have.status(200);
       console.log("Success");
       res.body.should.be.a('object');
       console.log("Success");
-      // console.log(res)
+      /
  
     }catch(error){
       console.log('ERROR in testcase',error);
